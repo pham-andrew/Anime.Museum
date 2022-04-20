9d595097d770faba2b66739cc8e87694f5f36a02
Andrew Pham <andrewdpham3@gmail.com>
2022-04-19 22:00:20 -0600

    virtual museum embedded

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dimensions, setDimensions] = useState({ height: window.innerHeight, width: window.innerWidth})
+
+  useEffect(() => {
+    function handleResize() {
+        setDimensions({
+            height: window.innerHeight,
+            width: window.innerWidth
+        })
+    }
+    window.addEventListener('resize', handleResize)
+    return _ => {
+        window.removeEventListener('resize', handleResize)
+    }
+   
+})
+  
+  return (
+    <iframe width={dimensions.width-3} height={dimensions.height-4} src="https://www.artsteps.com/embed/625e86630af57c094377b932/1280/720" frameborder="0" allowfullscreen></iframe>
+  );
+}
+
+export default App;
